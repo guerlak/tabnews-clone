ec4ffe440291cedd126fd337c44feca92f766a76
Guerlak <guerlak@gmail.com>
2026-01-06 22:57:31 -0300

    feat: config db and env variables

--- app/api/v1/status/route.js
+++ app/api/v1/status/route.js
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
+import database from "../../../../infra/database.js";
 
-export async function GET(req) {
+export async function GET() {
+  const res = await database.query("SELECT 1 + 1 as sum;");
+  console.log(res.rows);
   return NextResponse.json({ message: "aloha" });
 }

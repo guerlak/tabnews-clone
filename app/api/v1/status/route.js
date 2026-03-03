6237bb0c63155486f47a933d71890f377c312f2e
Guerlak <guerlak@gmail.com>
2026-03-03 11:56:48 -0300

    fix: test refactor

--- app/api/v1/status/route.js
+++ app/api/v1/status/route.js
@@ -2,7 +2,24 @@ import { NextResponse } from "next/server";
 import database from "@/infra/database.js";
 
 export async function GET() {
-  const res = await database.query("SELECT 1 + 1 as sum;");
-  console.log(res.rows);
-  return NextResponse.json({ message: "aloha" });
+  const updatedAt = new Date().toISOString();
+
+  const res = await database.query("SHOW server_version");
+  const server_version = res.rows[0].server_version;
+  const res1 = await database.query("SHOW max_connections");
+  const max_connections = res1.rows[0].max_connections;
+  const res2 = await database.query("SELECT count(*) FROM pg_stat_activity;");
+  const result2 = res2.rows[0];
+
+  return NextResponse.json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        status: 200,
+        max_connections,
+        current_connections: result2.count,
+        version: server_version,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to api/v1/status should return status 200", async () => {
-  const res = await fetch("http://localhost:3000/api/v1/status");
-  expect(res.status).toBe(200);
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  
+  expect(responseBody.dependencies.database).toEqual({
+    status: 200,
+    max_connections: "100",
+    current_connections: expect.any(String),
+    version: expect.any(String),
+  });
 });

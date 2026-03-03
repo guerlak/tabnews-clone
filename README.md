02c979a0e844ef940eb16a4a09c0d2bae0b78f13
Guerlak <guerlak@gmail.com>
2026-03-03 14:40:17 -0300

    fix: refactor on `database.js` db connection with try catch

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # tabnews-clone
 
 Curso.dev Project to practice programming skills
+
+Current Chapter: Day 22 Migrations
--- infra/database.js
+++ infra/database.js
@@ -8,12 +8,21 @@ async function query(queryObj) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  const result = await client.query(queryObj);
-  await client.end();
-  return result;
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObj);
+    return result;
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
   query,
 };
+
+///j&Fv8Kt7R4.jbi
